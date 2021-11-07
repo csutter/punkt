485a72b710a8422f7491454bcaedb1420b117558
Christian Sutter <git@doublesignal.com>
2021-11-07 17:46:37 +0000

    Add fzf key bindings for Debian

--- tag-devcontainer/bashrc.d/00_platform_overrides.sh
+++ tag-devcontainer/bashrc.d/00_platform_overrides.sh
@@ -1,4 +1,4 @@
 # Tool locations (sourced in the non-tagged Bash setup scripts)
 export DISTRO_GIT_COMPLETION=/usr/share/bash-completion/completions/git
 export DISTRO_GIT_PROMPT=/usr/lib/git-core/git-sh-prompt
-export DISTRO_FZF_KEY_BINDINGS=__TODO__
+export DISTRO_FZF_KEY_BINDINGS=/usr/share/doc/fzf/examples/key-bindings.bash

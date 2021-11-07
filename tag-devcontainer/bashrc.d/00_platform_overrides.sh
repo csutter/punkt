bfed88e2822637f0e56051c387f290877965434e
Christian Sutter <git@doublesignal.com>
2021-11-07 17:19:23 +0000

    Initial stab at dotfiles for devcontainer

---
+++ tag-devcontainer/bashrc.d/00_platform_overrides.sh
@@ -0,0 +1,4 @@
+# Tool locations (sourced in the non-tagged Bash setup scripts)
+export DISTRO_GIT_COMPLETION=/usr/share/bash-completion/completions/git
+export DISTRO_GIT_PROMPT=/usr/lib/git-core/git-sh-prompt
+export DISTRO_FZF_KEY_BINDINGS=__TODO__
---
+++ tag-devcontainer/install.sh
@@ -0,0 +1,12 @@
+# Get these dotfiles bootstrapped on a devcontainer
+# TODO: Generalise to allow them to run on RHEL-based containers too
+
+# Install rcm
+sudo apt install rcm
+
+# Set up the rcrc and install dotfiles
+ln -s $HOME/dotfiles/rcrc/rcrc.devcontainer $HOME/.rcrc
+rcup
+
+# Install any additional software that I need in devcontainers
+sudo apt install -y fzf git-extras gnupg2 tree

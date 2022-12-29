e1872e9fb0c59668d6f7f2aa75c790e46b64c182
Christian Sutter <git@doublesignal.com>
2022-12-29 17:21:37 +0000

    Fix issue with VSC devcontainers and 1PW SSH agent

--- tag-macos/bashrc.d/01_platform_overrides.sh
+++ tag-macos/bashrc.d/01_platform_overrides.sh
@@ -15,6 +15,11 @@ export GPG_TTY=$(tty)
 # Use VS Code as the default editor
 export EDITOR=code
 
+# Some tools don't properly pick up on 1Password SSH agent from `~/.ssh/config` (looking at you,
+# VS Code devcontainers), set it using env variable to get them working
+# cf. https://1password.community/discussion/132867/visual-studio-code-remote-development-server-and-1password-ssh-config
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
 # Load bash completions
 if [ -f $HOMEBREW_PREFIX/etc/bash_completion ]; then
 . $HOMEBREW_PREFIX/etc/bash_completion

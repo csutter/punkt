df95c61eb95fd34f41894acc316a157b2b875730
Christian Sutter <git@doublesignal.com>
2023-03-06 18:20:13 +0000

    Fix hardcoded arm64 brew location in platform overrides

--- tag-macos/bashrc.d/01_platform_overrides.sh
+++ tag-macos/bashrc.d/01_platform_overrides.sh
@@ -1,5 +1,5 @@
 # Load Homebrew shell environment
-eval $(/opt/homebrew/bin/brew shellenv)
+eval $(brew shellenv)
 
 # Tool locations (sourced in the non-tagged Bash setup scripts)
 export DISTRO_GIT_COMPLETION=$HOMEBREW_PREFIX/etc/bash_completion.d/git-completion.bash

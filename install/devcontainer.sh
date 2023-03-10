b6fe49cfd8666fd435bdf57256983aac1f93a495
Christian Sutter <git@doublesignal.com>
2023-03-10 14:30:05 +0000

    Don't run commands using sudo in root containers

--- install/devcontainer.sh
+++ install/devcontainer.sh
@@ -1,14 +1,20 @@
+# Some devcontainers may run as root/superuser and not have sudo available
+CONDITIONAL_SUDO=''
+if (( $EUID != 0 )); then
+    CONDITIONAL_SUDO='sudo'
+fi
+
 if grep Debian /etc/os-release > /dev/null; then
   echo "Bootstrapping dotfiles for Debian-based systems"
 
   # Install an updated git version from backports (bullseye ships with Git 2.30 which is too old to
   # support Git commit signing with SSH keys, which is a part of my workflow)
-  echo "deb http://deb.debian.org/debian bullseye-backports main" | sudo tee /etc/apt/sources.list.d/bullseye-backports.list
-  sudo apt update
-  sudo apt install -y git/bullseye-backports
+  echo "deb http://deb.debian.org/debian bullseye-backports main" | $CONDITIONAL_SUDO tee /etc/apt/sources.list.d/bullseye-backports.list
+  $CONDITIONAL_SUDO apt update
+  $CONDITIONAL_SUDO apt install -y git/bullseye-backports
 
   # Install rcm and additional software packages
-  sudo apt install -y rcm fzf git-extras gnupg2 tree
+  $CONDITIONAL_SUDO apt install -y rcm fzf git-extras gnupg2 tree
 
   # Install dotfiles using RCM
   rm $HOME/.rcrc || true
@@ -18,7 +24,7 @@ elif grep -e Ubuntu /etc/os-release > /dev/null; then
   echo "Bootstrapping dotfiles for Ubuntu-based systems"
 
   # Install rcm and additional software packages
-  sudo apt install -y rcm fzf git-extras gnupg2 tree
+  $CONDITIONAL_SUDO apt install -y rcm fzf git-extras gnupg2 tree
 
   # Install dotfiles using RCM
   rm $HOME/.rcrc || true

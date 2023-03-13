2cfc99c381ee933c1363f71ed48e83bf4607c355
Christian Sutter <git@doublesignal.com>
2023-03-13 16:41:57 +0000

    Cleanup macOS install script

--- install/macos.sh
+++ install/macos.sh
@@ -1,6 +1,8 @@
 echo "Bootstrapping dotfiles for macOS"
 
 # Ensure Homebrew environment is in place
+pathmunge "/opt/homebrew/bin" # arm64
+pathmunge "/usr/local/bin" # x86
 eval "$(brew shellenv)"
 
 # Set up rcrc
@@ -9,12 +11,3 @@ cp $HOME/Developer/punkt/rcrc/rcrc.macos $HOME/.rcrc
 
 # Install dotfiles
 rcup -f
-
-# Set up bash completion from Docker.app
-# TODO: Set this up for Homebrew Docker instead
-# [ -f $HOMEBREW_PREFIX/etc/bash_completion.d/docker.bash-completion ] || \
-#       ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion \
-#       $HOMEBREW_PREFIX/etc/bash_completion.d
-# [ -f $HOMEBREW_PREFIX/etc/bash_completion.d/docker-compose.bash-completion ] || \
-#       ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion \
-#       $HOMEBREW_PREFIX/etc/bash_completion.d

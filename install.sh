da1338d9177a2a4f0e971332ad0f560be55f2d86
Christian Sutter <git@doublesignal.com>
2023-03-17 12:30:14 +0000

    Ensure DOTFILES_DIR is exported for subscripts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-DOTFILES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export DOTFILES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Dotfiles are in '$DOTFILES_DIR'"
 
 if uname -a | grep Darwin > /dev/null; then

041f00b21ee68ef97003b9efed3da63b879fdab6
Christian Sutter <git@doublesignal.com>
2023-03-17 12:28:51 +0000

    Fix missing directories in install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DOTFILES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd
 echo "Dotfiles are in '$DOTFILES_DIR'"
 
 if uname -a | grep Darwin > /dev/null; then
-  . install/macos.sh
+  . $DOTFILES_DIR/install/macos.sh
 else
-  . install/devcontainer.sh
+  . $DOTFILES_DIR/install/devcontainer.sh
 fi

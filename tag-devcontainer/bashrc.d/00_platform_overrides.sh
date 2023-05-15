3da21384ffbfe1694ab5e7f18dde10f73a0d6730
Christian Sutter <git@doublesignal.com>
2023-05-15 14:45:44 +0100

    Tweak devcontainer config (incl. VS Code flag)

--- tag-devcontainer/bashrc.d/00_platform_overrides.sh
+++ tag-devcontainer/bashrc.d/00_platform_overrides.sh
@@ -1,10 +1,10 @@
-# Tool locations (sourced in the non-tagged Bash setup scripts)
+# Tool locations for Debian/Ubuntu devcontainers (sourced in the non-tagged Bash setup scripts)
 export DISTRO_GIT_COMPLETION=/usr/share/bash-completion/completions/git
 export DISTRO_GIT_PROMPT=/usr/lib/git-core/git-sh-prompt
 export DISTRO_FZF_KEY_BINDINGS=/usr/share/doc/fzf/examples/key-bindings.bash
 
 # Use VS Code as the default editor in devcontainers
-export EDITOR=code
+export EDITOR="code --wait"
 
 # Customise PS1 to show we're in a container
 export PROMPT_PREFIX="  "

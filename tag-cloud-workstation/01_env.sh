e56c6bfea167bfbf0b99efe325b65ca17b1b7e87
Christian Sutter <git@doublesignal.com>
2023-07-11 14:26:37 +0100

    Delete cloud workstation tag for now

--- tag-cloud-workstation/01_env.sh
+++
@@ -1,3 +0,0 @@
-export PROMPT_PREFIX=$'\u@\[\e[34;3m\]\h\u2601\[\e[0m\]:'
-export EDITOR="vim"
-export SYSTEM_TYPE="cloud-workstation"
--- tag-cloud-workstation/functions.sh
+++
@@ -1,5 +0,0 @@
-# r(): Jump to repos directory
-function r() {
-  cd "$HOME/repos/$1"
-}
-[ -d $HOME/repos ] && complete -W "$(ls $HOME/repos)" r

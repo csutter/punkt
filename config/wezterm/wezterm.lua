ae296b57630af5aa06977e08f9c243b9bd042bbc
Christian Sutter <git@doublesignal.com>
2025-01-06 13:39:46 +0000

    Basic wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Glacier'
+-- Look & feel
+config.color_scheme = 'GitHub Dark'
+
+-- Font configuration
+config.font = wezterm.font {
+  family = 'PragmataPro',
+  weight = 'Regular',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0', 'ss01=on' },
+}
+config.font_size = 15.0
+config.line_height = 1.2
 
 return config

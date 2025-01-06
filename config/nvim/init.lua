55a901d150675157fd7dddd9879fad56e8e97b49
Christian Sutter <git@doublesignal.com>
2025-01-06 14:20:57 +0000

    Some more basic vim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,8 +8,17 @@ vim.opt.mouse = 'a'           -- Enable mouse support
 vim.opt.ignorecase = true     -- Case insensitive search
 vim.opt.smartcase = true      -- But case sensitive when uppercase present
 vim.opt.hlsearch = true       -- Highlight search results
-vim.opt.wrap = false          -- Don't wrap lines
+vim.opt.wrap = true           -- Wrap lines
+vim.opt.colorcolumn = '100'   -- Ruler
+vim.opt.textwidth = 100       -- Soft wrap width
 vim.opt.breakindent = true    -- Preserve indentation in wrapped text
 vim.opt.tabstop = 2           -- Width of tab character
 vim.opt.shiftwidth = 2        -- Width of indent
 vim.opt.expandtab = true      -- Convert tabs to spaces
+
+-- File handling settings
+vim.opt.eol = true  -- Ensure files end with newline
+
+-- Basic shortcuts
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Write buffer' })
+vim.keymap.set('n', '<leader>vr', ':source $MYVIMRC<CR>', { desc = 'Re-source configuration' })

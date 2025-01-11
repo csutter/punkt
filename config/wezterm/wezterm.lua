16eb2871cf6e72ac1ffeb12dcdb44c91f4f8edfa
Christian Sutter <git@doublesignal.com>
2025-01-11 18:33:37 +0000

    Wezterm: tab bar

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,6 +3,9 @@ local config = wezterm.config_builder()
 
 -- Look & feel
 config.color_scheme = 'Dark+'
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
 
 -- Font configuration
 config.font = wezterm.font {

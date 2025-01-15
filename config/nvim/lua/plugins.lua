8a265809d37623a585219239e8e13e2beaf8950b
Christian Sutter <git@doublesignal.com>
2025-01-15 14:12:25 +0000

    Neovim: telescope mappings, plugin updates

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -48,11 +48,18 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require('telescope.builtin')
 
-      vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope oldfiles' })
+      vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = 'Telescope registers' })
+
+      vim.keymap.set('n', '<leader>gg', builtin.git_status, { desc = 'Telescope git status' })
     end
   }
 }

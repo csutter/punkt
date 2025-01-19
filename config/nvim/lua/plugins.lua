acf084f8cc7c9a9b526b6a4fbf815c136daa40a3
Christian Sutter <git@doublesignal.com>
2025-01-19 13:41:52 +0000

    Neovim: Setup mini.trailspace

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -67,12 +67,22 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-      require('mini.tabline').setup() 
+      require('mini.tabline').setup()
+
       require('mini.diff').setup({
         view = {
           style = 'sign',
           signs = { add = '▌', change = '▌', delete = '▁' },
         }
+      })
+
+      require('mini.trailspace').setup()
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*',
+        callback = function()
+          MiniTrailspace.trim()
+          MiniTrailspace.trim_last_lines()
+        end
       })
     end
   },

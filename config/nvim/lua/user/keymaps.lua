27db76f465bab894466af250c5ce3935e4bb65a5
Christian Sutter <git@doublesignal.com>
2025-01-20 14:07:36 +0000

    Neovim: easier window moving shortcuts

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -5,3 +5,15 @@ vim.keymap.set('n', '<leader>q', vim.cmd.close, { desc = 'Close window' })
 -- Toggles
 vim.keymap.set('n', '_/', vim.cmd.nohlsearch, { desc = 'Turn off highlight search' })
 vim.keymap.set('n', '_ ', ':set list!<CR>', { desc = 'Toggle list chars', silent = true })
+
+-- Window navigation
+vim.keymap.set('n', '<C-H>', '<C-w>h', { desc = 'Focus on left window' })
+vim.keymap.set('n', '<C-J>', '<C-w>j', { desc = 'Focus on below window' })
+vim.keymap.set('n', '<C-K>', '<C-w>k', { desc = 'Focus on above window' })
+vim.keymap.set('n', '<C-L>', '<C-w>l', { desc = 'Focus on right window' })
+
+-- Window resizing
+vim.keymap.set('n', '<C-S-h>', '<C-w><', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<C-S-j>', '<C-w>-', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<C-S-k>', '<C-w>+', { desc = 'Increase window height' })
+vim.keymap.set('n', '<C-S-l>', '<C-w>>', { desc = 'Increase window width' })

6e735da746e5a50cedf6b29b6d48f90fd73258ce
Christian Sutter <git@doublesignal.com>
2025-01-27 14:27:16 +0000

    Neovim: Slightly better terminal

--- config/nvim/lua/user/terminal.lua
+++ config/nvim/lua/user/terminal.lua
@@ -1,32 +1,56 @@
--- terminal.lua: Terminal-related configuration
+-- open_terminal(): Open a terminal split in the current tab
+--
+-- Opens a terminal split in the current tab, optionally with a command.
+local function open_terminal(command)
+  local win_height = vim.api.nvim_win_get_height(0)
+  local term_height = math.max(math.floor(win_height * 0.2), 10)
 
--- Open terminal
-vim.keymap.set('n', '<leader>tt', function()
-  -- Calculate 20% of total height
-  local height = math.min(10, math.floor(vim.o.lines * 0.2))
+  vim.cmd('botright ' .. term_height .. 'split')
 
-  -- Create split with specific height
-  vim.cmd('botright ' .. height .. 'split')
-  vim.cmd('terminal')
-  vim.cmd('startinsert')
-end, { desc = 'Open terminal in horizontal split' })
+  if command and command ~= '' then
+    vim.cmd('terminal ' .. command)
+  else
+    vim.cmd('terminal')
+  end
+end
 
--- Close terminal
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('t', '<S-Esc>', function()
-    vim.api.nvim_win_close(0, true)
-end, { desc = 'Close terminal window' })
+vim.api.nvim_create_user_command('Term', function(opts)
+  open_terminal(opts.args)
+end, {
+    nargs = '?',  -- Optional argument
+    desc = 'Open terminal (optional: with command)',
+  })
 
--- Hide line numbers in terminal mode
+vim.api.nvim_create_user_command('T', function(opts)
+  open_terminal(opts.args)
+end, {
+    nargs = '?',  -- Optional argument
+    desc = 'Open terminal (optional: with command)',
+  })
+vim.keymap.set('n', '<C-\\>', function() open_terminal() end, { noremap = true, silent = true })
+
+vim.keymap.set('t', '<C-\\><C-\\>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    vim.cmd('startinsert')
+  end
+})
+
 vim.api.nvim_create_autocmd('TermEnter', {
+  pattern = '*',
   callback = function()
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
-  end,
+  end
 })
+
 vim.api.nvim_create_autocmd('TermLeave', {
+  pattern = '*',
   callback = function()
     vim.opt_local.number = true
     vim.opt_local.relativenumber = true
-  end,
+  end
 })

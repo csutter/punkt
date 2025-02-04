0c6ada5e8956cf87b5299496861035e2c64a10ad
Christian Sutter <git@doublesignal.com>
2025-02-04 11:03:58 +0000

    Neovim: Fewer and more sensible Telescope keymaps

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -61,11 +61,8 @@ return {
     config = function()
       local builtin = require('telescope.builtin')
 
-      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader><tab>', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope oldfiles' })
-      vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = 'Telescope registers' })
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<C-/>', builtin.live_grep, { desc = 'Telescope live grep' })
     end
   },
   {

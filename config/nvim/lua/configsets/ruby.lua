a196b5a9d125ae5529d7259fc46e0671f2367d16
Christian Sutter <git@doublesignal.com>
2025-02-05 17:00:53 +0000

    Neovim: Split Ruby and Rails configsets

--- config/nvim/lua/configsets/ruby.lua
+++ config/nvim/lua/configsets/ruby.lua
@@ -1,7 +1,2 @@
 -- Add treesitter language support
 table.insert(_G.PunktConfig.settings.treesitter_ensure_installed, "ruby")
-table.insert(_G.PunktConfig.settings.treesitter_ensure_installed, "embedded_template")
-table.insert(_G.PunktConfig.settings.treesitter_ensure_installed, "html")
-table.insert(_G.PunktConfig.settings.treesitter_ensure_installed, "css")
-table.insert(_G.PunktConfig.settings.treesitter_ensure_installed, "scss")
-table.insert(_G.PunktConfig.settings.treesitter_ensure_installed, "javascript")

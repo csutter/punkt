47371629b5a62cd5cc72135b13a7a7ed4e35fb36
Christian Sutter <git@doublesignal.com>
2025-02-08 11:48:57 +0000

    Wezterm: Back to PragmataPro

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,12 +9,12 @@ config.hide_tab_bar_if_only_one_tab = false
 
 -- Font configuration
 config.font = wezterm.font {
-  family = 'Iosevka Term',
-  weight = 450,
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0', 'ss05=on' },
+  family = 'PragmataPro',
+  weight = 400,
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 }
 config.font_size = 15
-config.line_height = 1.25
+config.line_height = 1.4
 
 -- Shortcuts:
 config.debug_key_events = true

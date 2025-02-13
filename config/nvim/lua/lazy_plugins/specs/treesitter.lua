5560670fed6dc4c7b7b61f73d8b1e0ed904966e9
Christian Sutter <git@doublesignal.com>
2025-02-13 15:52:34 +0000

    Neovim: Set up treesitter languages

--- config/nvim/lua/lazy_plugins/specs/treesitter.lua
+++ config/nvim/lua/lazy_plugins/specs/treesitter.lua
@@ -1,5 +1,30 @@
--- TODO: Make me dynamic again!
-local languages = { 'bash', 'lua', 'markdown', 'vim' }
+-- Always install some core languages
+local core_languages = { 'bash', 'lua', 'markdown', 'vim' }
+
+-- Get project-specific languages from ENV
+local env = vim.env.NVIM_TREESITTER_LANGS or ""
+local env_languages = vim.split(env, ',', { trimempty = true })
+
+-- Merge several tables into one
+local function merge_list_tables(...)
+  local result = {}
+  local seen = {}
+
+  for i = 1, select("#", ...) do
+    local tbl = select(i, ...)
+    if type(tbl) == "table" then
+      for _, value in ipairs(tbl) do
+        if not seen[value] then
+          seen[value] = true
+          table.insert(result, value)
+        end
+      end
+    end
+  end
+
+  return result
+end
+
 
 return {
   {
@@ -9,7 +34,7 @@ return {
       local configs = require('nvim-treesitter.configs')
 
       configs.setup({
-        ensure_installed = languages,
+        ensure_installed = merge_list_tables(core_languages, env_languages),
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
